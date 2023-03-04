7698d51ec25d51cf50de6eb3af690c225928689e
anwarul <anwarulhoque032@gmail.com>
2023-03-04 23:09:16 +0600

    React State & Lifecycle in Class Component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Clock = ({locale}) => {
+export default class Clock extends Component {
+        //  Old way full-form
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { date: new Date() };
+  // }
+
+        // Old way short form 
+  state = { date: new Date() };
+  
+  componentDidMount() {
+    this.clockTimer = setInterval( () => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  } 
+  
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
     return (
-        <div>
-           <h1 className=''>
-            <span className='text'>{new Date().toLocaleTimeString(locale)}</span>
-            </h1> 
-        </div>
-    );
-};
+      <div>
+        <h1 className=''>
+            <span className='text'>{this.state.date.toLocaleTimeString(this.props.locale)}</span>
+        </h1> 
+      </div>
+    )
+  }
+}
 
-export default Clock;
+  
